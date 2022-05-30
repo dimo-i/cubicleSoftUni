b3f0d3bc0f660b8062bcfad18bffcb74094dda9a
dimo-i <81874988+dimo-i@users.noreply.github.com>
2022-05-30 12:46:30 +0300

    handlebars
    add handlebars

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 const express = require('express')
-// const handlebars = require('express-handlebars');
+const handlebars = require('express-handlebars');
 
 
 const app = express()
 
-// app.use('/static', express.static('public'));
+app.use('/static', express.static('public'));
 
-// app.engine('hbs', handlebars.engine({
-//     extname: 'hbs'
-// }));
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}));
 
-// app.set('view engine', 'hbs');
-// app.set('views', './src/views')
+app.set('view engine', 'hbs');
+app.set('views', './src/views')
 
 app.get('/', (req, res) => {
-    res.send('Hello')
-    // res.render('index');
+    res.render('index');
 
 });
 
