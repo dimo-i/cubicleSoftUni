79d2b9481699ce9fbce49c00b85fc2f5a89b2cb9
dimo-i <81874988+dimo-i@users.noreply.github.com>
2022-05-30 12:41:34 +0300

    update
    update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 const express = require('express')
+// const handlebars = require('express-handlebars');
+
 
 const app = express()
 
-app.use('/static', express.static('public'));
+// app.use('/static', express.static('public'));
+
+// app.engine('hbs', handlebars.engine({
+//     extname: 'hbs'
+// }));
+
+// app.set('view engine', 'hbs');
+// app.set('views', './src/views')
 
 app.get('/', (req, res) => {
-    res.send('Hellow World');
+    res.send('Hello')
+    // res.render('index');
 
 });
 
