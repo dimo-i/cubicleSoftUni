ed41cfd4218f09da468246738a1254ce772ec104
dimo-i <dimo.i4254@gmail.com>
2022-05-30 11:49:15 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hellow World');
+
+});
+
+app.listen(5000, () => console.log(`App is listening on port 5000`));
+
+
