caa1bcd272d70d342c0eadcd910084086f3f2b10
dimo-i <81874988+dimo-i@users.noreply.github.com>
2022-06-06 14:32:16 +0300

    add attach accessory endpoint

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -33,4 +33,8 @@ router.get('/details/:id', async (req, res) => {
     res.render('details', { cube })
 });
 
+router.get('/:cubeId/attach-accessory', (req, res) => {
+    res.render('accessory/attach');
+})
+
 module.exports = router;
