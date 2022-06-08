67a2de52abe7f368fe525d55b8b4caa02cd02376
dimo-i <81874988+dimo-i@users.noreply.github.com>
2022-06-08 14:58:24 +0300

    fix search cube

--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -6,16 +6,27 @@ const Cube = require('../models/Cube')
 
 exports.getAll = async (search = '', fromInput, toInput) => {
 
-    let cubes = await Cube.find().lean();
+    const from = Number(fromInput) || 0
+    const to = Number(toInput) || 6
 
 
-    // const from = Number(fromInput) || 0
-    // const to = Number(toInput) || 6
+
+    // let cubes = await Cube.find(
+    //     {
+    //         name: {$regex: new RegExp(search, 'i')},
+    //         difficultyLevel: {$and: [{$gte: from}, {$lte: to}]}
+    //     },
+    //     ).lean();
+
     // const result = cubes
-    // .filter(x => x.name.toLowerCase().includes(search.toLowerCase()))
-    // .filter(x => x.difficultyLevel >= from && x.difficultyLevel <= to)
+    //     .filter(x => x.name.toLowerCase().includes(search.toLowerCase()))
+    //     .filter(x => x.difficultyLevel >= from && x.difficultyLevel <= to)
 
-     return cubes;
+    let cubes = await Cube.find({name: {$regex: new RegExp(search, 'i')}})
+    .where('difficultyLevel').lt(to).gte(from)
+    .lean();
+
+    return cubes;
 };
 
 exports.getOne = (cubeId) => Cube.findById(cubeId).populate('accessories');
@@ -35,10 +46,10 @@ exports.create = (cube) => Cube.create(cube);
 exports.attachAccessory = async (cubeId, accessoryId) => {
     const cube = await Cube.findById(cubeId);
     const accessory = await Accessory.findById(accessoryId);
-    
+
     cube.accessories.push(accessory);
     accessory.cubes.push(cube);
-    
+
     await cube.save();
     await accessory.save();
 
