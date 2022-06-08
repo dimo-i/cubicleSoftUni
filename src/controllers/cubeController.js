656f7658cda5b29ba29050738a0f92306d7965b7
dimo-i <81874988+dimo-i@users.noreply.github.com>
2022-06-08 14:35:25 +0300

    fix accessories attaching/visualising

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -36,7 +36,8 @@ router.get('/details/:id', async (req, res) => {
 
 router.get('/:cubeId/attach-accessory', async (req, res) => {
     const cube = await cubeService.getOne(req.params.cubeId).lean();
-    const accessories = await accessoryService.getAll().lean()
+
+    const accessories = await accessoryService.getAllWithout(cube.accessories).lean()
     res.render('accessory/attach', {cube, accessories});
 })
 
--- src/services/accessoryService.js
+++ src/services/accessoryService.js
@@ -2,4 +2,6 @@ const Accessory = require('../models/Accessory');
 
 exports.getAll = () => Accessory.find()
 
+exports.getAllWithout = (ids) => Accessory.find({_id: {$nin: ids}});
+
 exports.create = (accessoryData) => Accessory.create(accessoryData);
